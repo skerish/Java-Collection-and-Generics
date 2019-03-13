89a884524952a45cb1526bf5807f3695ee5cbeeb
kerish kumar <31753397+skerish@users.noreply.github.com>
2019-03-13 10:40:25 +0530

    circular linkedLlist 2.2

---
+++ src/circularList/circularLinkedList.java
@@ -0,0 +1,46 @@
+package circularList;
+
+public class circularLinkedList {
+
+    int size = 0;
+    Node head = null;
+    Node tail = null;
+
+    class Node{
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public void setHead(Node head) {
+        this.head = head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public void setTail(Node tail) {
+        this.tail = tail;
+    }
+}
